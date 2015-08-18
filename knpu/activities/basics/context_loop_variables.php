bcb962f38584494bb48ff9d20ce7c14ddf580771
Ryan Weaver <ryan@thatsquality.com>
2015-08-18 16:47:20 -0400

    Removing all the old activities

--- knpu/activities/basics/context_loop_variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$parentContext = require __DIR__.'/context_print_variables.php';
-
-return array_merge($parentContext, array(
-    'tags' => array('foo', 'bar', 'baz'),
-));
--- knpu/activities/basics/context_print_variables.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$productName = 'Awesome Product';
-$productPrice = 15.99;
-
-return array(
-    'name'  => $productName,
-    'price' => $productPrice,
-);
